700d7d62c0ea78cdc0718d9cead5b85727974442
sujalshrestha10 <165934225+sujalshrestha10@users.noreply.github.com>
2024-04-03 22:35:30 +0545

    Add files via upload

---
+++ B. Modal Window/script.js
@@ -0,0 +1,26 @@
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".close-modal");
+const btnsOpenModal = document.querySelectorAll(".show-modal");
+console.log(btnsOpenModal);
+
+const OpenModal = function () {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+
+const closeModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  btnsOpenModal[i].addEventListener("click", OpenModal);
+  btnCloseModal.addEventListener("click", closeModal);
+  overlay.addEventListener("click", closeModal);
+}
+document.addEventListener("keydown", function (event) {
+  console.log(event.key);
+  if (event.key === "Escape" && !modal.classList.contains("hidden")) {
+    closeModal();
+  }
+});
